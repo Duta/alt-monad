8eefcf46d8c6d3a67b7c5a3a31c9f86077f95e66
Bertie Wheen <wheen.b@gmail.com>
2014-12-03 17:18:09 +0000

    Functors, endofunctors, and 'normal' functors (Hask endofunctors)

---
+++ AltMonad/Functor.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+module AltMonad.Functor where
+
+import AltMonad.Category
+import AltMonad.Hask
+
+class (Category c, Category d)
+   => Functor c d f where
+  map :: a `c` b -> f a `d` f b
+
+class    Functor a a f => Endofunctor a f
+instance Functor a a f => Endofunctor a f
+
+class    Endofunctor Hask f => HaskellFunctor f
+instance Endofunctor Hask f => HaskellFunctor f
