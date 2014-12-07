5433ebe75cfbe530d722251b5a3d712c290a30ed
Bertie Wheen <wheen.b@gmail.com>
2014-12-07 19:50:51 +0000

    Morphisms from 'normal' monoids to HaskellMonoids

--- AltMonad/HaskellMonoid.hs
+++ AltMonad/HaskellMonoid.hs
@@ -6,8 +6,9 @@ module AltMonad.HaskellMonoid where
 
 import AltMonad.Hask
 import AltMonad.Monoid
+import Data.Function (const)
 import qualified Data.Monoid as Normal
-import Data.Tuple (curry)
+import Data.Tuple (curry, uncurry)
 
 class Monoid () (,) Hask m
    => HaskellMonoid m where
@@ -23,3 +24,9 @@ instance HaskellMonoid m
       => Normal.Monoid m where
   mempty  = empty
   mappend = (<>)
+
+midDefault :: Normal.Monoid m => () -> m
+midDefault = const Normal.mempty
+
+mcombDefault :: Normal.Monoid m => (m, m) -> m
+mcombDefault = uncurry Normal.mappend
